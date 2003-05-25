3d7626afdadbe4a8053464a0754c07405d82b38a
Bart De Schuymer <bdschuym@pandora.be>
2003-05-25 10:44:17 +0000

    *** empty log message ***

--- kernel/linux2.5/net/bridge/netfilter/ebt_802_3.c
+++ kernel/linux2.5/net/bridge/netfilter/ebt_802_3.c
@@ -18,7 +18,6 @@ static int ebt_filter_802_3(const struct sk_buff *skb, const struct net_device *
 	struct ebt_802_3_info *info = (struct ebt_802_3_info *)data;
 	struct ebt_802_3_hdr *hdr = (struct ebt_802_3_hdr *)skb->mac.ethernet;
 	uint16_t type = hdr->llc.ui.ctrl & IS_UI ? hdr->llc.ui.type : hdr->llc.ni.type;
-	
 
 	if (info->bitmask & EBT_802_3_SAP) {
 		if (FWINV(info->sap != hdr->llc.ui.ssap, EBT_802_3_SAP)) 
@@ -26,7 +25,7 @@ static int ebt_filter_802_3(const struct sk_buff *skb, const struct net_device *
 		if (FWINV(info->sap != hdr->llc.ui.dsap, EBT_802_3_SAP))
 				return EBT_NOMATCH;
 	}
-	 
+
 	if (info->bitmask & EBT_802_3_TYPE) {
 		if (!(hdr->llc.ui.dsap == CHECK_TYPE && hdr->llc.ui.ssap == CHECK_TYPE))
 			return EBT_NOMATCH;
@@ -73,4 +72,3 @@ static void __exit fini(void)
 module_init(init);
 module_exit(fini);
 MODULE_LICENSE("GPL");
-
